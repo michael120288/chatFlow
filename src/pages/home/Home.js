d1d7b3836fa47e399b25d947da21f1e1e5375c10
michael120288 <Michael.sheptun@gmail.com>
2026-01-31 23:15:29 -0500

    add button qa practice and navbar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -14,8 +14,24 @@ const Home = () => {
     }
   };
 
+  const handleQAPracticeClick = () => {
+    // Navigate to QA Practice page
+    navigate('/qa-practice');
+  };
+
   return (
     <div className="home-container">
+      <nav className="home-navbar">
+        <div className="navbar-brand">Chatty</div>
+        <div className="navbar-buttons">
+          <button onClick={handleChatClick} className="navbar-button">
+            Chat
+          </button>
+          <button onClick={handleQAPracticeClick} className="navbar-button">
+            QA Practice
+          </button>
+        </div>
+      </nav>
       <div className="home-content">
         <h1 className="home-title">Welcome to Chatty</h1>
         <p className="home-description">Connect with friends and start chatting</p>
